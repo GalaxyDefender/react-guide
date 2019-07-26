ae4bf007fefb4a0bca37c20a3ab2a0caea427ca2
GalaxyDefender <qgerard.gerard@gmail.com>
2019-07-26 09:45:00 +0200

    return array output via map()

--- src/App.js
+++ src/App.js
@@ -50,14 +50,11 @@ class App extends Component {
     if( this.state.showPerson ) {
       persons = (
         <div>
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age}/> 
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Quent')}
-            changed={this.nameChangedHandler}>My Hobbies: Painting</Person> 
+          {this.state.persons.map(person => {
+            return <Person 
+              name={person.name}
+              age={person.age}/>
+          })}
         </div>
       )
     }
