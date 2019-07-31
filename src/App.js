11805e328fa4bcd0f4b769408b1cceb9ee787510
GalaxyDefender <qgerard.gerard@gmail.com>
2019-07-31 08:36:52 +0200

    removed Radium for enabling CSS modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Radium from 'radium';
+// import Radium, { StyleRoot } from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -63,10 +63,10 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
+      // ':hover': {
+      //   backgroundColor: 'lightgreen',
+      //   color: 'black'
+      // }
     };
 
     let persons = null;
@@ -86,10 +86,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      };
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // };
     };
 
     const classes = [];
@@ -101,17 +101,20 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, this is Quent</h1>
-        <p className={classes.join(' ')}>This is a text</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonHandler}>Toggle Persons</button>
-          {persons}
-      </div>
+      // <StyleRoot>
+        <div className="App">
+          <h1>Hi, this is Quent</h1>
+          <p className={classes.join(' ')}>This is a text</p>
+          <button 
+            style={style}
+            onClick={this.togglePersonHandler}>Toggle Persons</button>
+            {persons}
+        </div>
+      // </StyleRoot>
+      
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
    }
 }
 
-export default Radium(App);
+export default App;
